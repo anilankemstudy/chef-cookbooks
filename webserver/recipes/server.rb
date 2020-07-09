e012491161caefb32db73a321bf11d2e6c8a4293
anil ankem study <anilankemstudy@gmail.com>
2020-07-09 13:07:35 +0000

    remote file added to webserver

--- webserver/recipes/server.rb
+++ webserver/recipes/server.rb
@@ -13,15 +13,21 @@ package 'apache2' do
 end
 
 
-template '/var/www/html/index.html' do
-	source 'index.html.erb'
+cookbook_file  '/var/www/html/index.html' do
+	source 'test.html'
 	owner 'root'
     group 'root'
     mode '0755'
     action :create
 end
 
-
+remote_file '/var/www/html/gopal.jg' do
+    source 'https://gopalimagebucket.s3.us-east-2.amazonaws.com/happyface.jpg'
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+end
 service 'apache2' do
     action [ :enable, :start ]
 end
